fc259d724d54547116ca3da91d0839562f97aae3
KUSHAL-HC <kushalhcfrom1974@gmail.com>
2026-02-26 23:58:44 +0530

    done with the creating files and organising the files into a sequential order

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,19 @@
 const connectToMongo = require("./db");
 connectToMongo();
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
+app.listen(port, () => {
+    console.log(` Server running at http://localhost:${port}`);
+    // if u do not fet this above console then run node index.js inside a new terminal
+  });
+
