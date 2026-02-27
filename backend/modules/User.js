9553762465ed619d6a5a17be94493f51b28c2176
KUSHAL-HC <kushalhcfrom1974@gmail.com>
2026-02-27 23:38:49 +0530

    setting the gmail to get a unique input where there should be no duplications

--- backend/modules/User.js
+++ backend/modules/User.js
@@ -16,4 +16,7 @@ const UserSchema = new Schema({
         default:Date.now
     }
   });
-  module.exports = mongoose.model('user',UserSchema)
+
+  const user = mongoose.model('user',UserSchema);
+  user.createIndexes();
+  module.exports=user;
