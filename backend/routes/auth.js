24bdf100ae64d8a9cc762a0024007074641a09b0
KUSHAL-HC <kushalhcfrom1974@gmail.com>
2026-03-05 19:36:43 +0530

    done with the login credentials

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,9 +6,7 @@ const bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
 
 const JWT_SECRET = "mysecretkey";
-router.post(
-  '/createuser',
-  [
+router.post('/createuser',[
     body('name').isLength({ min: 3 }),
     body('email').isEmail(),
     body('password').isLength({ min: 5 }),
@@ -47,4 +45,39 @@ router.post(
   }
 );
 
+//Authenticate a user using:POST "/api/auth/login".No login required
+router.post('/login',[
+    body('email','Enter a valid email').isEmail(),
+    body('password','Password cannot be blank').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {email,password} = req.body;
+    try{
+        let user =await User.findOne({email});
+        if(!user)
+        {
+            return res.status(400).json({eroor:"Please try to login with correct credentials"});
+        }
+
+        const passwordCompare =await bcrypt.compare(password,user.password);
+        if(!passwordCompare)
+        {
+            return res.status(400).json({error:"Please try to login with with correct ceadentials"});
+        }
+        const data = {
+            user:{
+                id:user.id
+            }
+        }
+        const authtoken = jwt.sign(data,JWT_SECRET);
+        res.json({authtoken});
+    } catch (err) {
+        res.status(500).send({ error: "Server error" });
+      }
+  })
 module.exports = router;
