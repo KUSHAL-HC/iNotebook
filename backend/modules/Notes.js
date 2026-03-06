39a62f82344fd80d51e0499f938c0306a35a81ed
KUSHAL-HC <kushalhcfrom1974@gmail.com>
2026-03-06 23:17:04 +0530

    done with the notes entering into the particular user

--- backend/modules/Notes.js
+++ backend/modules/Notes.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
+const User = require('./User');
 const { Schema } = mongoose;
 
 const NotesSchema = new Schema({
-  title: {
+User:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'user'
+},title: {
     type: String,
     required: true
   },
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const router = express.Router();
+var fetchuser = require('../middleware/fetchuser');
+const Notes = require('../modules/Notes');
+const { body, validationResult } = require('express-validator');
 
-router.get('/', (req, res) => {
-  res.json({ msg: "Notes route working" });
+//ROUTE 1: Get All the Notes using GET "/api/notes/getuser".Login required
+router.get('/fetchallnotes',fetchuser,async (req, res) => {
+  const notes = await Notes.find({user:req.user.id});
+  res.json(notes);
+});
+
+//ROUTE 2:Get All the Notes using post "/api/notes/addnotes"
+router.post('/addnote',fetchuser,[
+  body('title').isLength({ min: 3 }),
+  body('description').isLength({min:5}),
+],async (req, res) => {
+  try{
+
+    const {title,description,tag} = req.body;
+    //if there are errors ,return bad requests and errors
+    const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+        }
+        const note = new Notes({title,description,tag,user:req.user.id})
+        const savedNote = await note.save();
+  res.json(savedNote);
+  }catch(error){
+      res.status(500).json({ error: "Server error" });
+  }
 });
 
 module.exports = router;
