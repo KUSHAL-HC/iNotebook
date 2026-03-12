7d1dbd330d3b55b39ccfea103c3178fd2ae7fb84
KUSHAL-HC <kushalhcfrom1974@gmail.com>
2026-03-12 23:13:17 +0530

    react router set up is done for the INoteBook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Navbar from './components/Navbar.js';
+import Home from './components/Home.js';
+import About from './components/About.js';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is iNotebook</h1>
-    </div>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/About" element={<About />} />
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 export default App;
